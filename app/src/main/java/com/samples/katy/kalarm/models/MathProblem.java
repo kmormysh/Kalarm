82cb865c24d76552c97057b7bfbae61f60bc3790
kmormysh <kate.morm@gmail.com>
2015-04-06 17:09:36 +0300

    cleanup MathProblem

--- app/src/main/java/com/samples/katy/kalarm/models/MathProblem.java
+++ app/src/main/java/com/samples/katy/kalarm/models/MathProblem.java
@@ -4,41 +4,18 @@ import java.util.Random;
 import java.util.Stack;
 
 public class MathProblem {
-    public int getAnswer() {
-        return answer;
-    }
-
-    public String getMathProblem() {
-        return mathProblem;
-    }
-
-    private enum Operators {
-        ADD, SUBTRACT, DIVIDE, MULTIPLY;
-
-        @Override
-        public String toString() {
-            String operator = "";
-            switch (ordinal()) {
-                case 0:
-                    operator = "+";
-                    break;
-                case 1:
-                    operator = "-";
-                    break;
-                case 2:
-                    operator = "/";
-                    break;
-                case 3:
-                    operator = "*";
-                    break;
-            }
-            return operator;
-        }
-    }
 
     private int answer;
     private String mathProblem;
     private Stack<String> stack = new Stack<>();
+
+    private final String OP_ADD = "+";
+    private final String OP_SUBTRACT = "-";
+    private final String OP_DIVIDE = "/";
+    private final String OP_MULTIPLY = "*";
+
+    private final String[] ADD_AND_SUBTRACT = {OP_ADD, OP_SUBTRACT};
+    private final String[] DIVIDE_AND_MULTIPLY = {OP_DIVIDE, OP_MULTIPLY};
 
     public MathProblem(int difficulty) {
         mathProblem = "";
@@ -47,13 +24,21 @@ public class MathProblem {
         solveStack();
     }
 
+    public int getAnswer() {
+        return answer;
+    }
+
+    public String getMathProblem() {
+        return mathProblem;
+    }
+
     private void createStack(int difficulty) {
         Random random = new Random();
 
         if (difficulty == 1) {
-            generateNumbersForAdditionOrSubtraction(Operators.values()[random.nextInt(2)]);
+            generateNumbersForAdditionOrSubtraction(ADD_AND_SUBTRACT[random.nextInt(2)]);
         } else if (difficulty == 2) {
-            String operator = Operators.values()[random.nextInt(2)].toString();
+            String operator = ADD_AND_SUBTRACT[random.nextInt(2)];
             stack.push(operator);
             int number = random.nextInt(100);
 
@@ -61,28 +46,31 @@ public class MathProblem {
             mathProblem += Integer.toString(number) + operator;
 
             stack.push(Integer.toString(number));
-            generateNumbersForDivisionOrMultiplication(Operators.values()[random.nextInt(2) + 2]);
+            generateNumbersForDivisionOrMultiplication(DIVIDE_AND_MULTIPLY[random.nextInt(2)]);
         } else if (difficulty == 3) {
-            String operator = Operators.values()[random.nextInt(2)].toString();
+            String operator = ADD_AND_SUBTRACT[random.nextInt(2)];
             stack.push(operator);
 
-            generateNumbersForDivisionOrMultiplication(Operators.values()[random.nextInt(2) + 2]);
+            generateNumbersForDivisionOrMultiplication(DIVIDE_AND_MULTIPLY[random.nextInt(2)]);
 
             //Add values to final problem
             mathProblem += operator;
 
-            generateNumbersForDivisionOrMultiplication(Operators.values()[random.nextInt(2) + 2]);
+            generateNumbersForDivisionOrMultiplication(DIVIDE_AND_MULTIPLY[random.nextInt(2)]);
+        }
+        else {
+            throw new UnsupportedOperationException();
         }
     }
 
-    private void generateNumbersForAdditionOrSubtraction(Operators operator) {
+    private void generateNumbersForAdditionOrSubtraction(String operator) {
         Random random = new Random();
-        stack.push(operator.toString());
+        stack.push(operator);
         int number = random.nextInt(100);
         stack.push(Integer.toString(number));
 
         //Add values to final problem
-        mathProblem += number + operator.toString();
+        mathProblem += number + operator;
 
         number = random.nextInt(100);
         stack.push(Integer.toString(number));
@@ -91,32 +79,30 @@ public class MathProblem {
         mathProblem += number;
     }
 
-    private void generateNumbersForDivisionOrMultiplication(Operators operator) {
+    private void generateNumbersForDivisionOrMultiplication(String operator) {
         Random random = new Random();
         int numerator, denominator, result;
         denominator = random.nextInt(10) + 1;
         result = random.nextInt(10) + 1;
         numerator = denominator * result;
 
-        stack.push(operator.toString());
-        if (operator == Operators.DIVIDE) {
+        stack.push(operator);
+        if (operator.equals(OP_DIVIDE)) {
             stack.push(Integer.toString(numerator));
             stack.push(Integer.toString(denominator));
 
             //Add values to final problem
-            mathProblem += Integer.toString(numerator) + operator.toString()
+            mathProblem += Integer.toString(numerator) + operator
                     + Integer.toString(denominator);
 
-        } else if (operator == Operators.MULTIPLY) {
+        } else if (operator.equals(OP_MULTIPLY)) {
             stack.push(Integer.toString(result));
             stack.push(Integer.toString(denominator));
 
             //Add values to final problem
-            mathProblem += Integer.toString(result) + operator.toString()
+            mathProblem += Integer.toString(result) + operator
                     + Integer.toString(denominator);
         }
-
-
     }
 
     private void solveStack() {
@@ -132,25 +118,19 @@ public class MathProblem {
             }else{
                 integerOne = Integer.valueOf(newStack.pop());
                 integerTwo = Integer.valueOf(newStack.pop());
-                int result = token.compareTo(Operators.ADD.toString()) == 0 ? integerTwo + integerOne :
-                        token.compareTo(Operators.SUBTRACT.toString()) == 0 ? integerTwo - integerOne :
-                                token.compareTo(Operators.MULTIPLY.toString()) == 0 ? integerTwo * integerOne :
+                int result = token.compareTo(OP_ADD) == 0 ? integerTwo + integerOne :
+                        token.compareTo(OP_SUBTRACT) == 0 ? integerTwo - integerOne :
+                                token.compareTo(OP_MULTIPLY) == 0 ? integerTwo * integerOne :
                                         integerTwo / integerOne;
 
                 newStack.push(String.valueOf(result));
             }
         }
-
         answer = Integer.valueOf(newStack.pop());
     }
 
     private boolean isOperator(String token) {
-        if (Operators.ADD.toString().equals(token)
-                || Operators.SUBTRACT.toString().equals(token)
-                || Operators.MULTIPLY.toString().equals(token)
-                || Operators.DIVIDE.toString().equals(token)) {
-            return true;
-        }
-        return false;
+        return OP_ADD.equals(token) || OP_SUBTRACT.equals(token) ||
+                OP_DIVIDE.equals(token) || OP_MULTIPLY.equals(token);
     }
 }
